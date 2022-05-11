7608ac884925337b984bdd42f2fba4f602da99fb
y.yilmaz1976 <y.yilmaz1976@gmail.com>
2022-05-11 17:25:46 +0300

    second project added

---
+++ python/hands-on/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/Flask_If_for_structure/app.py
@@ -0,0 +1,20 @@
+from tkinter.font import names
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def head():
+#    first = "This is my first conditions experience"
+#    return render_template("index.html", message=first)
+
+
+@app.route("/")
+def header():
+    names = ['Serdar', 'David', 'Taylor', "Furkan"]
+    return render_template("index.html", object=names)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
